976f04b37c4e151b44b5f79bc40a25b79a677acc
gijs-epping <gijs.epping@shopworks.nl>
2019-09-17 17:04:09 +0200

    Update

--- src/api.py
+++ src/api.py
@@ -48,7 +48,7 @@ def add_task():
         row.status = 'Inbox'
         row.url = url
         
-        row.add_new(TextBlock, title="asdasdasdjkhasdkha sdkhas djkhas dkjahs dkasjhd")
+        row.children.add_new(TextBlock, title="asdasdasdjkhasdkha sdkhas djkhas dkjahs dkasjhd")
 
 
         return 'Succeceed in adding task', 200
